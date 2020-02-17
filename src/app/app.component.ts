168a5fc3df6a107674b7e02d54182dbdaec7a967
Eduardo Ivan Piña Gdc <dossosllac@gmail.com>
2020-02-17 17:46:00 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
+import { mainContentAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    mainContentAnimation(),
+  ]
 })
 export class AppComponent {
-  title = 'Phoenix';
+  sidebarState: string;
+
+  constructor(
+    private sidebarService: SidebarService,
+  ){}
+
+  ngOnInit(){
+    this.sidebarService.sidebarStateObservable$
+      .subscribe((newState: string) => {
+        this.sidebarState = newState;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSidenavModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { ToggleSidebarModule } from './modules/toggle-sidebar/toggle-sidebar.module';
+import { HeaderModule } from './modules/header/header.module';
+import { SidebarModule } from './modules/sidebar/sidebar.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    DashboardModule,
+    HeaderModule,
+    SidebarModule,
+    ToggleSidebarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
